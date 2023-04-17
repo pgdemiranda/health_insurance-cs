0856154c99549bd75a4ea62d118a604fabebbcfe
Pablo Miranda <pgdemiranda@gmail.com>
2023-04-17 19:10:00 -0300

    port fixed

--- handler.py
+++ handler.py
@@ -36,11 +36,10 @@ def health_insurance_predict():
         # prediction
         df_response = pipeline.get_prediction(model, test_raw, df3)
         
-        return df_response.to_json(orient='records')
+        return df_response
     
     else:
         return Response('{}', status=200, mimetype='application/json')
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run( '0.0.0.0', debug=True )
