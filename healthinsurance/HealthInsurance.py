b794d493bf96b8f23a502d83754ae0732824fa25
Pablo Miranda <pgdemiranda@gmail.com>
2023-04-19 22:53:15 -0300

    deployment problem fixed

--- healthinsurance/HealthInsurance.py
+++ healthinsurance/HealthInsurance.py
@@ -4,13 +4,12 @@ import pandas as pd
 
 class HealthInsurance():
     def __init__(self):
-        self.home_path = ''
-        self.annual_premium_scaler = pickle.load(open(self.home_path + 'src/features/annual_premium_scaler.pkl', 'rb'))
-        self.age_scaler = pickle.load(open(self.home_path + 'src/features/age_scaler.pkl', 'rb'))
-        self.vintage_scaler = pickle.load(open(self.home_path + 'src/features/vintage_scaler.pkl', 'rb'))
-        self.target_encode_gender_scaler = pickle.load(open(self.home_path + 'src/features/target_encode_gender_scaler.pkl', 'rb'))
-        self.target_encode_region_code_scaler = pickle.load(open(self.home_path + 'src/features/target_encode_region_code_scaler.pkl', 'rb'))
-        self.fe_policy_sales_channel_scaler = pickle.load(open(self.home_path + 'src/features/fe_policy_sales_channel_scaler.pkl', 'rb'))
+        self.annual_premium_scaler = pickle.load(open('src/features/annual_premium_scaler.pkl', 'rb'))
+        self.age_scaler = pickle.load(open('src/features/age_scaler.pkl', 'rb'))
+        self.vintage_scaler = pickle.load(open('src/features/vintage_scaler.pkl', 'rb'))
+        self.target_encode_gender_scaler = pickle.load(open('src/features/target_encode_gender_scaler.pkl', 'rb'))
+        self.target_encode_region_code_scaler = pickle.load(open('src/features/target_encode_region_code_scaler.pkl', 'rb'))
+        self.fe_policy_sales_channel_scaler = pickle.load(open(+ 'src/features/fe_policy_sales_channel_scaler.pkl', 'rb'))
 
     def data_cleaning(self, df1):
         # 1.1. Rename Columns
